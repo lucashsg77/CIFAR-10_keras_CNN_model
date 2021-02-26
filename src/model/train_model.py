b84c31ddd8d872fcfc20ac03e7785a9b4846fc7f
lucashsg77 <57928235+lucashsg77@users.noreply.github.com>
2021-02-26 23:46:59 +0000

    minor changes

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -5,12 +5,12 @@ from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
 from tensorflow.keras import regularizers, optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import to_categorical
-
+# the cifar-10 dataset has 32x32 images from 10 differente classes with 6000 images per class, giving us 50k for training and 10k for validation
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
+# normalizing the data
 x_train = x_train / 255
 x_test = x_test / 255
-
+# since we have no ordinary relationship between the classes we need to encode some of the data so we don't get unexpected results
 y_train_cat = to_categorical(y_train, 10)
 y_test_cat = to_categorical(y_test, 10)
 
@@ -40,19 +40,22 @@ model.add(Flatten())
 model.add(Dense(128, activation = 'relu', kernel_initializer = 'he_uniform'))
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
+# we use softmax because we are working with 10 different classes
 model.add(Dense(10, activation = 'softmax'))
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-# shifts the image accross width and height and flips the image horizantally.
-ImgDataGen = ImageDataGenerator(width_shift_range = 0.1, height_shift_range = 0.1, horizontal_flip = True, rotation_range = 20)
+# since we needed more images because the previous models were overfitting I'm using keras to flip and shift the image a little bit
+# so we can have more data for the training
+ImgDataGen = ImageDataGenerator(width_shift_range = 0.1, height_shift_range = 0.2, vertical_flip = True, rotation_range = 15)
 it_train = ImgDataGen.flow(x_train, y_train_cat)
 steps = int(x_train.shape[0] / 64)
+
+#gives us a summary of the model layers, trainable and non-trainable parameters as well as the total parameters
 model.summary()
 history = model.fit(it_train, epochs = 55, steps_per_epoch = steps, validation_data = (x_test, y_test_cat))
+# prints the final acc of the model
 evaluation = model.evaluate(x_test, y_test_cat)
-print('Test Accuracy: {}'.format(evaluation[1]))
+print('Final accuracy: {}'.format(evaluation[1]))
 
-_,acc=model.evaluate(test_X,test_Y)
-print(acc * 100)
 model_name = "cifar_model_" + epochs + "epoch.h5"
 model.save(model_name)
